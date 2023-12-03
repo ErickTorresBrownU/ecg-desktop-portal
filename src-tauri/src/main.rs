eb55962507e5536ad6648c08b12b31c39ab54622
Erick Torres <erick_torres@brown.edu>
2023-12-03 01:14:17 -0500

    refactor: remove unnecessary thread spawn

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -106,125 +106,123 @@ macro_rules! nullify_and_skip {
 }
 
 fn main_backend(app_handle: AppHandle) {
-    thread::spawn(move || {
-        let mut csv_writer: Option<csv::Writer<File>> = None;
-        let mut csv_writer_been_flushed = false;
+    let mut csv_writer: Option<csv::Writer<File>> = None;
+    let mut csv_writer_been_flushed = false;
 
-        let mut time_offsets: Option<(i64, i64)> = None;
+    let mut time_offsets: Option<(i64, i64)> = None;
 
-        let mut serial_port: Option<Box<dyn SerialPort>> = None;
-        let mut time_of_last_ok = Instant::now();
+    let mut serial_port: Option<Box<dyn SerialPort>> = None;
+    let mut time_of_last_ok = Instant::now();
 
-        const MAX_TIME_WITHOUT_VERIFICATION_MILLIS: u64 = 1000;
-        const VERIFICATION_INTERVAL_MILLIS: u64 = MAX_TIME_WITHOUT_VERIFICATION_MILLIS / 2;
+    const MAX_TIME_WITHOUT_VERIFICATION_MILLIS: u64 = 1000;
+    const VERIFICATION_INTERVAL_MILLIS: u64 = MAX_TIME_WITHOUT_VERIFICATION_MILLIS / 2;
 
-        loop {
-            if serial_port.is_none() {
-                if let Some(ref mut csv_writer) = csv_writer {
-                    if !csv_writer_been_flushed {
-                        // TODO: handle this result
-                        dbg!("Flushing");
-                        let _ = csv_writer.flush();
+    loop {
+        if serial_port.is_none() {
+            if let Some(ref mut csv_writer) = csv_writer {
+                if !csv_writer_been_flushed {
+                    // TODO: handle this result
+                    dbg!("Flushing");
+                    let _ = csv_writer.flush();
 
-                        csv_writer_been_flushed = true;
-                    }
-                }
-
-                let ports = serialport::available_ports().unwrap();
-
-                if ports.len() == 0 {
-                    continue;
-                }
-
-                app_handle.emit_all("reset-monitor", ()).unwrap();
-
-                let port_config = serialport::new(&ports.get(0).unwrap().port_name, 57600)
-                    .timeout(Duration::from_secs(3));
-
-                match port_config.open() {
-                    Ok(port) => {
-                        serial_port = Some(port);
-
-                        csv_writer = Some(csv::Writer::from_path(setup_csv_file()).unwrap());
-                        csv_writer_been_flushed = false;
-                    }
-                    Err(_) => {
-                        nullify_and_skip!(serial_port);
-                    }
-                };
-            }
-
-            if time_of_last_ok.elapsed().as_millis() >= VERIFICATION_INTERVAL_MILLIS.into() {
-                time_of_last_ok = Instant::now();
-
-                let ok_state = serial_port.as_mut().unwrap().write("OK\n".as_bytes());
-
-                if ok_state.is_err() {
-                    dbg!("Couldn't Write");
-
-                    nullify_and_skip!(serial_port);
+                    csv_writer_been_flushed = true;
                 }
             }
 
-            let line = if let Ok(line) = read_line_from_serial(&mut serial_port) {
-                line
-            } else {
-                nullify_and_skip!(serial_port);
-            };
+            let ports = serialport::available_ports().unwrap();
 
-            let parsed_entry = parse_serial_entry(line.trim());
-
-            if parsed_entry.is_err() {
-                app_handle
-                    .emit_all(
-                        "new-reading",
-                        EcgReading {
-                            milliseconds: Local::now().timestamp_millis(),
-                            value: 0.,
-                        },
-                    )
-                    .unwrap();
-
+            if ports.len() == 0 {
                 continue;
             }
 
-            if let Ok(non_offset_reading) = parsed_entry {
-                if time_offsets.is_none() {
-                    time_offsets = Some((
-                        non_offset_reading.milliseconds,
-                        chrono::Local::now().timestamp_millis(),
-                    ));
+            app_handle.emit_all("reset-monitor", ()).unwrap();
+
+            let port_config = serialport::new(&ports.get(0).unwrap().port_name, 57600)
+                .timeout(Duration::from_secs(3));
+
+            match port_config.open() {
+                Ok(port) => {
+                    serial_port = Some(port);
+
+                    csv_writer = Some(csv::Writer::from_path(setup_csv_file()).unwrap());
+                    csv_writer_been_flushed = false;
                 }
+                Err(_) => {
+                    nullify_and_skip!(serial_port);
+                }
+            };
+        }
 
-                let time_offsets = time_offsets.unwrap();
+        if time_of_last_ok.elapsed().as_millis() >= VERIFICATION_INTERVAL_MILLIS.into() {
+            time_of_last_ok = Instant::now();
 
-                let offset_reading = EcgReading {
-                    milliseconds: non_offset_reading.milliseconds - time_offsets.0 + time_offsets.1,
-                    value: non_offset_reading.value,
-                };
+            let ok_state = serial_port.as_mut().unwrap().write("OK\n".as_bytes());
 
-                // let test = EcgReading {
-                //     milliseconds: Local::now().timestamp_millis(),
-                //     value: non_offset_reading.value,
-                // };
+            if ok_state.is_err() {
+                dbg!("Couldn't Write");
 
-                app_handle
-                    .emit_all("new-reading", non_offset_reading)
-                    .unwrap();
-
-                let date: DateTime<Local> = DateTime::from_naive_utc_and_offset(
-                    NaiveDateTime::from_timestamp_millis(offset_reading.milliseconds).unwrap(),
-                    *Local::now().offset(),
-                );
-
-                csv_writer
-                    .as_mut()
-                    .unwrap()
-                    .write_record(&[date.to_rfc3339(), offset_reading.value.to_string()])
-                    .unwrap();
+                nullify_and_skip!(serial_port);
             }
         }
-    });
+
+        let line = if let Ok(line) = read_line_from_serial(&mut serial_port) {
+            line
+        } else {
+            nullify_and_skip!(serial_port);
+        };
+
+        let parsed_entry = parse_serial_entry(line.trim());
+
+        if parsed_entry.is_err() {
+            app_handle
+                .emit_all(
+                    "new-reading",
+                    EcgReading {
+                        milliseconds: Local::now().timestamp_millis(),
+                        value: 0.,
+                    },
+                )
+                .unwrap();
+
+            continue;
+        }
+
+        if let Ok(non_offset_reading) = parsed_entry {
+            if time_offsets.is_none() {
+                time_offsets = Some((
+                    non_offset_reading.milliseconds,
+                    chrono::Local::now().timestamp_millis(),
+                ));
+            }
+
+            let time_offsets = time_offsets.unwrap();
+
+            let offset_reading = EcgReading {
+                milliseconds: non_offset_reading.milliseconds - time_offsets.0 + time_offsets.1,
+                value: non_offset_reading.value,
+            };
+
+            // let test = EcgReading {
+            //     milliseconds: Local::now().timestamp_millis(),
+            //     value: non_offset_reading.value,
+            // };
+
+            app_handle
+                .emit_all("new-reading", non_offset_reading)
+                .unwrap();
+
+            let date: DateTime<Local> = DateTime::from_naive_utc_and_offset(
+                NaiveDateTime::from_timestamp_millis(offset_reading.milliseconds).unwrap(),
+                *Local::now().offset(),
+            );
+
+            csv_writer
+                .as_mut()
+                .unwrap()
+                .write_record(&[date.to_rfc3339(), offset_reading.value.to_string()])
+                .unwrap();
+        }
+    }
 }
 
 fn main() {
